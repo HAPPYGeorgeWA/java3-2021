4b3a9e3b4927a1f442cee8d9019513b14e975530
HAPPYGeorgeWA <godspeed246@gmail.com>
2021-04-11 10:58:24 +0800

    practice

--- src/main/java/com/HG/Package/Tester.java
+++ src/main/java/com/HG/Package/Tester.java
@@ -7,13 +7,13 @@ public class Tester {
         Box3 box3 = new Box3();
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("Please enter object\'s length: ");
-        int length = Integer.parseInt(scan.next());
-        System.out.println("Please enter object\'s width: ");
-        int width = Integer.parseInt(scan.next());
-        System.out.println("Please enter object\'s height: ");
-        int height = Integer.parseInt(scan.next());
-        System.out.println("length: " + length + ", width: " + width + ", height: " + height);
+        System.out.println("Please enter length: ");
+        int length = scan.nextInt();
+        System.out.println("Please enter width: ");
+        int width = scan.nextInt();
+        System.out.println("Please enter height: ");
+        int height = scan.nextInt();
+        System.out.println("length:"+length+" width: "+width+" height: "+height);
         if (box3.validate(length, width, height)) {
             System.out.println(box3.getName());
         }
