013a4bf01810f400fea4e5dc8a20dc90c946c894
HAPPYGeorgeWA <godspeed246@gmail.com>
2021-06-01 15:39:00 +0800

    CarWin

---
+++ src/main/java/com/Car/CarWin.java
@@ -0,0 +1,18 @@
+package com.Car;
+
+import jdk.xml.internal.JdkXmlFeatures;
+
+import javax.swing.*;
+
+public class CarWin {
+
+    static final int width=400;
+    public static void main(String[] args) {
+        JFrame frame= new JFrame();
+        frame.setSize(width,122);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+
+
+    }
+}
